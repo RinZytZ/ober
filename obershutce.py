e2b67c7983b227f5d3e0e991146d8ffd2c3c695c
RinZytZ <rinzyou1607@gmail.com>
2026-05-24 11:32:33 +0400

    obershutce.py

--- obershutce.py
+++ obershutce.py
@@ -28,7 +28,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="хоум-", intents=intents)
 
 @bot.event
 async def on_ready():
